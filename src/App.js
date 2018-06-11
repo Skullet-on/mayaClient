c39b5fdc89f05fbdb33a6ead8761729cd92ed71d
Eugene <skullet-on@mail.ru>
2018-06-11 13:22:00 +0300

    added fetch to api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: []
+    };
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData(){
+    fetch('/api/status')
+      .then(res => res.json())
+      .then(parsedJson => this.setState(parsedJson))
+      .catch(error => console.log('parsung failed', error))
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          {this.state.status}
         </p>
       </div>
     );
