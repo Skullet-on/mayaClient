fe452ad92acdc1f933a95d009b2b31e31881c44e
Eugene <skullet-on@mail.ru>
2018-06-26 12:51:17 +0300

    added handleSuccess and handle Error

--- src/components/news/news-list/news-list.jsx
+++ src/components/news/news-list/news-list.jsx
@@ -2,49 +2,53 @@ import React, { Component } from 'react';
 import NewsItem from '../news-item/news-item';
 
 class NewsList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newsList: [],
-      status: []
-    };
-  }
-  
-  componentDidMount() {
-    this.fetchData();
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			newsList: []
+		};
+	}
+	
+	componentDidMount() {
+		this.fetchData();
+	}
 
-  fetchData(){
-    fetch('/api/news')
-      .then(res => { this.handleResponse(res) })
-      .catch(error => console.log('parsing failed', error))
-  }
+	fetchData(){
+		fetch('/api/news')
+			.then(res => { this.handleResponse(res) })
+			.catch(error => console.warn('parsing failed', error))
+	}
 
-  handleResponse(res){
-    console.log(res.status);
-    const status = res.status === 200
-    ? res.json().then(response => this.setState({
-      newsList: response, 
-      status: res.status
-    }))
-      .catch(error => console.log('parsing failed', error))
-    : this.setState({status: res.status})
-  }
+	handleResponse(res) {
+		res.status === 200
+			? this.handleSuccess(res)
+			: this.handleError(res)
+	}
 
-  render() {
-    return (
-      <div>
-        {
-          this.state.newsList.map((news) => 
-            <NewsItem key={news.id} news={news} />
-          )
-        }
-        {
-          this.state.status
-        }
-      </div>
-    );
-  }
+	handleSuccess(res) {
+		res.json().then(response => this.setState({
+			newsList: response
+		}))
+	}
+
+	handleError(res) {
+		console.warn(res.status)
+	}
+
+	render() {
+		return (
+			<div>
+				{
+					this.state.newsList.map((news) => 
+						<NewsItem key={news.id} news={news} />
+					)
+				}
+				{
+					this.state.status
+				}
+			</div>
+		);
+	}
 }
 
 export default NewsList;
