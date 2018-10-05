268e24476c74bbf8ef28562daf9b24df73b4f4d5
unknown <skullet-on@mail.ru>
2018-10-05 13:44:23 +0300

    added new pages

--- src/components/faq/faq-item/index.jsx
+++ src/components/faq/faq-item/index.jsx
@@ -23,7 +23,7 @@ export default class FaqItem extends Component{
 	renderCollapsed() {
 		return <div className="item">
 			<div onClick={(e) => this.handleClick(e)} className="question"><h1>Q. {this.props.faq.question}</h1><h1>-</h1></div>
-			<div className="answer"><h2>A. {this.props.faq.answer}</h2></div>
+			<div className="answer">A. {this.props.faq.answer}</div>
 		</div>
 	}
 
